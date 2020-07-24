13c28ada8781d968e08b3690b9f9a6fc6bbc31bd
janielabad <janielveniceabad@gmail.com>
2020-07-24 16:39:58 -0700

    Revert "Set up Auth0 signup/login"
    This reverts commit b307ebac063a8130bf886bb704f4b55f711c28b9.

--- server.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
-const app = require('./app');
-
-const server = app.listen(process.env.PORT, () => {
-  console.log(`app running on port ${process.env.PORT}...`);
-});
