62a4af510a4f4e7c3e6a7ff24b62ff49318bcc2e
janielabad <janielveniceabad@gmail.com>
2020-07-31 14:15:42 -0700

    Connect to database

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const dotenv = require('dotenv');
+const mysql = require('mysql');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: 'languegg',
+});
+
+db.connect((err) => {
+  if (err) {
+    return console.log('Error connecting to the database.');
+  }
+
+  console.log('Database connection successful!');
+});
+
 const server = app.listen(process.env.PORT, () => {
-  console.log(`app running on port ${process.env.PORT}...`);
+  console.log(`App running on port ${process.env.PORT}...`);
 });
