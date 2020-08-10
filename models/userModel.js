5658caf8578bc4916d05623fa6ece66d5286a204
janielabad <janielveniceabad@gmail.com>
2020-08-10 16:17:35 -0700

    Update user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,11 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize, Sequelize) => {
   return sequelize.define('user', {
-    user_id: DataTypes.INTEGER,
-    name: DataTypes.STRING,
+    user_id: DataTypes.STRING(35),
+    known_language: DataTypes.STRING(5),
+    nickname: {
+      type: DataTypes.STRING(15),
+      isAlphanumeric: true,
+    },
   });
 };
