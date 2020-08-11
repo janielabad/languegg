8ccfc121af5ce937920aa4128c616bda04e56aa6
janielabad <janielveniceabad@gmail.com>
2020-08-10 19:45:26 -0700

    Create util to catch errors from async functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,13 @@ const db = require('../db');
 const User = db.users;
 const Op = db.Sequelize.op; // for operators
 
-exports.getAllUsers = async (req, res, next) => {
+const catchAsyncErr = require('../utils/catchAsyncErr');
+
+exports.getAllUsers = catchAsyncErr(async (req, res, next) => {
   const users = await User.findAll();
 
   console.log(users);
   res.status(200).json({
     status: 'success',
   });
-};
+});
